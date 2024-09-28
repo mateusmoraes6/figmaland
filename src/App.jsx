94c16d0b96b5826140ce408354bff40a5356cb80
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-09-28 11:33:32 -0300

    testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { CalendarFeature } from "./components/CalendarFeature";
+import { Testimonials } from "./components/Testimonials";
 
 export default function App() {
   return (
     <div className="bg-dark-Background">
+        <Testimonials />
         <CalendarFeature />
     </div>
   )
